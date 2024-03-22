671560f6893df0837484ed59188c993336650d69
Patrick Darwinkel <patrick@darwinkel.net>
2024-03-22 15:31:42 +0100

    explorer on mobile UI

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,11 +15,11 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.Explorer()),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.MobileOnly(Component.Explorer()),
   ],
   left: [
     Component.PageTitle(),
@@ -64,7 +64,7 @@ Component.Graph({
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(), Component.MobileOnly(Component.Explorer())],
+  beforeBody: [Component.MobileOnly(Component.Explorer()), Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta())],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
